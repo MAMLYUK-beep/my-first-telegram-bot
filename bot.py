cd5e239197aad1248ab8424d0555b2c4bf42aff8
MAMLYUK-beep <luxxifeita@gmail.com>
2026-08-30 23:26:11 +0200

    Add /start and /help commands

--- bot.py
+++ bot.py
@@ -1,9 +1,16 @@
 from telegram import Update
-from telegram.ext import Application, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, MessageHandler,CommandHandler, filters, ContextTypes
 from config import TOKEN
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я твой первый бот 🤖")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я твой бот-помощник. Напиши /help, чтобы узнать, что я умею.")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Доступные команды:\n/start — начать\n/help — это меню")
 app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help_command))
 app.add_handler(MessageHandler(filters.TEXT, reply))
 app.run_polling()
 
